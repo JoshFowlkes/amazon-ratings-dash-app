ac66e7c4ed153c8acd98ae95df0c7ce09d78e32d
Josh Fowlkes <52584954+JoshFowlkes@users.noreply.github.com>
2019-08-26 12:26:31 -0600

    Update run.py

--- run.py
+++ run.py
@@ -26,7 +26,7 @@ sticky (string, optional): Stick the navbar to the top or the bottom of the view
 """
 
 navbar = dbc.NavbarSimple(
-    brand='YOUR APP NAME',
+    brand='Amazon Ratings Predictions',
     brand_href='/', 
     children=[
         dbc.NavItem(dcc.Link('Predictions', href='/predictions', className='nav-link')), 
@@ -45,10 +45,10 @@ footer = dbc.Container(
             html.P(
                 [
                     html.Span('Your Name', className='mr-2'), 
-                    html.A(html.I(className='fas fa-envelope-square mr-1'), href='mailto:<you>@<provider>.com'), 
-                    html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/<you>/<repo>'), 
-                    html.A(html.I(className='fab fa-linkedin mr-1'), href='https://www.linkedin.com/in/<you>/'), 
-                    html.A(html.I(className='fab fa-twitter-square mr-1'), href='https://twitter.com/<you>'), 
+                    html.A(html.I(className='fas fa-envelope-square mr-1'), href='mailto:thejoshfowlkes@gmail.com'), 
+                    html.A(html.I(className='fab fa-github-square mr-1'), href='https://github.com/JoshFowlkes'), 
+                    html.A(html.I(className='fab fa-linkedin mr-1'), href='https://www.linkedin.com/in/josh-fowlkes-18844018a/'), 
+                    html.A(html.I(className='fab fa-twitter-square mr-1'), href='https://twitter.com/beyond_brawn1'), 
                 ], 
                 className='lead'
             )
